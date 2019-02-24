15e1fb575b90f7157ab1d9f6dfa4cbb04e7bc7c0
Darya <d-kru27@ya.ru>
2019-02-24 17:08:48 +0300

    small refactoring

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-// window.onscroll = function() {scrollFunction()};
-//
-// function scrollFunction() {
-//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//         document.getElementById("myBtn").style.display = "block";
-//     } else {
-//         document.getElementById("myBtn").style.display = "none";
-//     }
-// }
 $(window).scroll(function () {
     var scrolled = $(window).scrollTop();
-console.log (scrolled);
-    if(scrolled  > 400) {
-        $(".arrow-top").show();
-    } else {
-        $(".arrow-top").hide();
-    }
+    (scrolled  > 400) ? $(".arrow-top").show(): $(".arrow-top").hide();
 });
 
 
